7fdbb36da8efd2acbbe98689f894cfc26eebe403
meowsl <VlaFrank@ya.ru>
2023-11-07 17:10:13 +0300

    styles:new pages

---
+++ nuxt-base/pages/date-form/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <VContainer
+    class="form d-flex flex-column justify-space-between"
+    style="border: 2px solid black !important; width: 75%; height: 80%;"
+  >
+    <div class="form__content-head px-16">
+      <p
+        class="form__title text-h5 font-weight-bold text-center py-6"
+        style="color: #BB1F1B !important"
+      >Записаться на прием</p>
+      <VDivider />
+    </div>
+    <div class="form__content">
+      <VContainer class="w-50">
+        <p class="text-h6 text-center text-black">Выберите дату и время</p>
+
+      </VContainer>
+    </div>
+    <div class="form__content-foot d-flex flex-column justify-center px-16">
+      <VDivider />
+      <div class="d-flex flex-row justify-start align-center text-start">
+        <NuxtLink
+          to="/"
+          class="d-flex flex-row align-center py-6"
+        >
+          <VImg
+            :src="backArrow"
+            width="40"
+            height="40"
+          />
+          <p class="ms-4 text-h6">Назад</p>
+        </NuxtLink>
+      </div>
+    </div>
+  </VContainer>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import backArrow from 'images/backArrow.svg'
+
+const fromDateVal = ref()
+const minDate = new Date()
+
+</script>
