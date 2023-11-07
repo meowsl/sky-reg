90283aea92c3b32117af36a8854ef36441d15ada
meowsl <126591397+meowsl@users.noreply.github.com>
2023-11-07 21:24:37 +0300

    Delete nuxt-base directory

--- nuxt-base/middleware/auth.ts
+++
@@ -1,6 +0,0 @@
-// export default defineNuxtRouteMiddleware(() => {
-//   const authStore = useAuthStore()
-
-//   if (!authStore.isAuth)
-//     return navigateTo('/login')
-// })
--- nuxt-base/models/applications.ts
+++
@@ -1,9 +0,0 @@
-export interface Applications{
-  id: number;
-  lastName: string;
-  firstName:string;
-  middleName:string;
-  phone:string;
-  dateApplication:Date;
-  typePriem:string;
-}
